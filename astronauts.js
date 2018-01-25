930b00042c9729c44d9c66a0f71935478b89fc4b
stefanobaggetto <stefano.baggetto@studenti.unitn.it>
2018-01-25 16:31:44 +0100

    fatta la simpleget

---
+++ astronauts.js
@@ -0,0 +1,49 @@
+const express = require('express'),
+    bodyParser = require('body-parser');
+const astronauts = express.Router()
+
+var uuid = require('uuid-v4'); //crea id univoci
+
+const arrayAstronauts = [] //array di oggetti
+
+//presentazione
+astronauts.route('/')
+  .get((req, res) => {
+    res.status(200)
+    res.json({message: 'Welcome to the API'})
+  });
+
+astronauts.route('/astronauts')
+  //get di tutto l'array
+    .get((req, res) => {
+    res.status(200)
+    res.json(arrayAstronauts)
+  })
+//posta un elemento
+  .post((req, res) => {
+    var astronaut = {}
+    astronaut.id = uuid()
+    if (req.body.firstName) astronaut.firstName = req.body.firstName
+    if (req.body.lastName) astronaut.lastName = req.body.lastName
+    if (req.body.isInSpace) {
+      astronaut.isInSpace = true;
+    } else {
+      astronaut.isInSpace = false;
+    }
+    arrayAstronauts.push(astronaut)
+    res.status(200)
+    res.json(astronaut)
+  });
+
+astronauts.route('/astronauts/:id')
+  .get((req, res) => {
+    var id = req.params.id
+    const i = arrayAstronauts.findIndex(item => {return item.id === id})
+    if (i==-1) res.sendStatus(404)
+    else{
+      res.status(200)
+      res.json(arrayAstronauts[i])
+    }
+  });
+
+module.exports = astronauts
